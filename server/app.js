cec5e4a078f95f27051e7abb3fd5b87261c6e5aa
victoriakapelush <victoriakapelush@gmail.com>
2024-03-28 12:42:02 -0400

    Add app.js

---
+++ server/app.js
@@ -0,0 +1,74 @@
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const session = require('express-session');
+const passport = require('passport');
+const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const cors = require('cors');
+require("dotenv").config();
+
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+
+const app = express();
+
+app.use(cors({
+  origin: 'https://chatsphere-3sxt.onrender.com', 
+  credentials: true
+}));
+
+mongoose.set('strictQuery', false);
+
+// Connect to MongoDB
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Connected to MongoDB');
+    const PORT = process.env.PORT || 8000
+    app.listen(PORT, () => {
+        console.log(`App is Listening on PORT ${PORT}`);
+    })
+  })
+  .catch(error => {
+    console.error('Error connecting to MongoDB:', error);
+  });
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'client/dist')));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// Add session middleware
+app.use(session({
+  secret: 'cats',
+  resave: false,
+  saveUninitialized: false
+}));
+  
+// Initialize passport and session
+app.use(passport.initialize());
+app.use(passport.session());
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
