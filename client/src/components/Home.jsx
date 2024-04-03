65902dea3c0c0ca82e77110662b362e2bfbc0b10
victoriakapelush <victoriakapelush@gmail.com>
2024-04-03 15:50:12 -0400

    Add home page

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -3,6 +3,14 @@ import user from '../assets/icons/user.png'
 import home from '../assets/icons/home.png'
 import hashtag from '../assets/icons/hashtag.png'
 import notifications from '../assets/icons/notifications.png'
+import icon from '../assets/icons/butterfly.svg';
+import comment from '../assets/icons/comment.png';
+import repost from '../assets/icons/repost.png';
+import sendpost from '../assets/icons/send-post.png';
+import likebefore from '../assets/icons/like-before.png';
+import random from '../assets/images/random.jpg'
+// eslint-disable-next-line no-unused-vars
+import likeafter from '../assets/icons/like-after.png';
 
 function Home() {
     return (
@@ -36,17 +44,39 @@ function Home() {
             <a href='/home' className='new-post-btn'>New Post</a>
         </div>
         <div className='profile-center'>
-
+            <header className='mini-header flex-row'>
+                <img src={icon}></img>
+            </header>
+            <div className='flex-row mini-header-btns-container'>
+                <Link to="/home" className='mini-header-btn'>Following</Link>
+                <Link to="/home" className='mini-header-btn'>Discover</Link>
+            </div>
+            <div className='post flex-row'>
+                <img className='profile-pic' src={icon}></img>
+                <div className='flex-column post-box'>
+                    <Link to='/home' className='link-to-profile'>Phil Lewis @phillewis.bsky.social·4h</Link>
+                    <p className='post-text'>Put a heating pad on one of the cat hammocks and it is like stuffing them in a get along shirt</p>
+                    <img className='post-image' src={random}></img>
+                    <div className='flex-row post-icons-container'>
+                        <Link to='/home'><img className='post-icon' src={comment} /></Link>
+                        <Link to='/home'><img className='post-icon' src={repost} /></Link>
+                        <Link to='/home'><img className='post-icon' src={sendpost} /></Link>
+                        <Link to='/home'><img className='post-icon' src={likebefore} /></Link>
+                    </div>
+                </div>
+            </div>
         </div>
         <div className='profile-right flex-column'>
-            <Link to="/home">Following</Link>
-            <Link to="/home">Discover</Link>
-            <Link to="/home">More feeds</Link>
-            <ul className='flex-row'>
-                <li>Feedback</li>
-                <li>Privacy</li>
-                <li>Terms</li>
-                <li>Help</li>
+            <div className='flex-column more-feed-links'>
+                <Link to="/home" className='feed-link'>Following</Link>
+                <Link to="/home" className='feed-link'>Discover</Link>
+                <Link to="/home" className='feed-link'>More feeds</Link>   
+            </div>
+            <ul className='flex-row terms-list'>
+                <li className='no-bullets'><a href='/home'>Feedback</a></li>
+                <li><a href='/home'>Privacy</a></li>
+                <li><a href='/home'>Terms</a></li>
+                <li><a href='/home'>Help</a></li>
             </ul>
         </div>
       </div>
