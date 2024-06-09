2962764eb968d2e6f4bda12ef108e2dd55446279
victoriakapelush <victoriakapelush@gmail.com>
2024-06-09 14:38:20 -0400

    Add background img holder to profile

--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 import HomeNav from './HomeNav'
 import HomeExtra from './HomeExtra'
 import back from '../assets/icons/back.png'
-import emoji from '../assets/images/emoji.png'
+import backgroundImage from '../assets/images/random.jpg'
 
 function Profile() {
     return (
@@ -19,9 +19,11 @@ function Profile() {
                         <span>0 posts</span>
                     </div>
                 </header>
-                <div className='background-image-holder'></div>
+                <div className='background-image-holder'>
+                    <img src={backgroundImage}/>
+                </div>
                 <div className='profile-photo-container flex-row'>
-                    <img src={emoji}/>
+                    <img src={backgroundImage}/>
                     <div className='flex-row subscribe-panel'>
                         <svg viewBox="0 0 24 24" aria-hidden="true"><g><path d="M3 12c0-1.1.9-2 2-2s2 .9 2 2-.9 2-2 2-2-.9-2-2zm9 2c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2zm7 0c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2z"></path></g></svg>
                         <svg viewBox="0 0 24 24" aria-hidden="true"><g><path d="M22 5v2h-3v3h-2V7h-3V5h3V2h2v3h3zm-.86 13h-4.241c-.464 2.281-2.482 4-4.899 4s-4.435-1.719-4.899-4H2.87L4 9.05C4.51 5.02 7.93 2 12 2v2C8.94 4 6.36 6.27 5.98 9.3L5.13 16h13.73l-.38-3h2.02l.64 5zm-6.323 0H9.183c.412 1.164 1.51 2 2.817 2s2.405-.836 2.817-2z"></path></g></svg>                    
